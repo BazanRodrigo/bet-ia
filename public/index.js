4d164ad56d732c1535e3d69f65c9805031f62365
BazanRodrigo <bazanzuritarodrigo@gmail.com>
2022-05-13 08:04:51 -0500

    first commit

---
+++ public/index.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import Logo from './assets/logobetia.png';
+
+class Index extends Component {
+    render() {
+        return(
+        <div>
+            <img src={process.env.PUBLIC_URL+"./logobetia.pg"} width="192" alt="logo"></img>
+        </div>
+        )
+    }
+}
+
+export default Index;
